d1a59c3d637ce5262503340410a3b46540c4a63e
julitofm93 <94149241+julitofm93@users.noreply.github.com>
2021-11-19 17:50:32 -0300

    Update index.js

--- public/index.js
+++ public/index.js
@@ -2,9 +2,20 @@ document.addEventListener('submit', event=>{
     event.preventDefault();
     let form = document.getElementById('formulario');
     let data = new FormData(form);
+    let title = data.get('title')
+    let price = data.get('price')
+    let thumbnail = data.get('thumbnail')
+    let req = {
+        title: title,
+        price: price,
+        thumbnail: thumbnail
+    }
     fetch('http://localhost:8080/api/productos',{
         method:'POST',
-        body:data
+        body:JSON.stringify(req),
+        headers:{
+            "Content-type":"application/json"
+        }
     }).then(result=>{
         return result.json();
     }).then(json=>{
